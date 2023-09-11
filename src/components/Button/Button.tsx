4058c6a6ad2d670853c81896d4f6f5aceb4211d8
Butcherok <Butcherok231@gmail.com>
2023-09-11 18:02:50 +0300

    add: button props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,9 +2,21 @@ import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
-export const Button = ({ children, appearance }: ButtonProps) => {
-  return <button className={cn(styles.button, {
-    [styles.primary]: appearance == 'primary',
-    [styles.ghost]: appearance == 'ghost',
-  })}>{children}</button>;
+export const Button = ({
+  children,
+  appearance,
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={cn(styles.button, className, {
+        [styles.primary]: appearance == 'primary',
+        [styles.ghost]: appearance == 'ghost',
+      })}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
