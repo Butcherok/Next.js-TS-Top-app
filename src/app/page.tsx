0ee3087ed266b8bc3e7835d36a70f8a2848cdca2
Butcherok <Butcherok231@gmail.com>
2023-09-11 17:48:46 +0300

    add: Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Htag, TheFooter, TheHeader } from '@/components';
+import { Htag, TheFooter, TheHeader, Button } from '@/components';
 
 export default function Home() {
   return (
     <main>
       <TheHeader />
       <Htag tag="h1">text Htag</Htag>
+      <Button appearance='primary'>Button</Button>
+      <Button appearance='ghost'>Button</Button>
+
       <TheFooter />
     </main>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { TheHeader } from './TheHeader/TheHeader';
 import { TheFooter } from './TheFooter/TheFooter';
 import { Htag } from './Htag/Htag';
+import { Button } from './Button/Button';
 
-export { TheHeader, TheFooter, Htag };
+export { TheHeader, TheFooter, Htag, Button };
